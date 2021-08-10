41b4d6e9ae55cdef54274274008051ac364b79bf
Senthilvadivel-20 <senthilsubramaniyam20@gmail.com>
2021-08-10 17:44:32 +0530

    first commit

--- ipl/urls.py
+++ ipl/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
+# from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
