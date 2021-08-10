aa378fbc87d2bb482c97fdf82d9b81bae320dc4f
Senthilvadivel-20 <senthilsubramaniyam20@gmail.com>
2021-08-11 00:34:42 +0530

    settings updated

--- ipl/urls.py
+++ ipl/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from .import views
 
-# from django.views.static import serve
-# from django.conf.urls import url
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
-    path('result/',views.result,name='result')
+    path('result/',views.result,name='result'),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
