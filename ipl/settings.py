4dc5a889b615aff15f721efd9ec223aa070f980a
Senthilvadivel-20 <senthilsubramaniyam20@gmail.com>
2021-08-10 23:33:39 +0530

    Procfile

--- ipl/settings.py
+++ ipl/settings.py
@@ -122,11 +122,12 @@ STATIC_URL = '/static/'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
    
-]
+# ]
 
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
 
 
