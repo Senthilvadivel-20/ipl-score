c91de472960746b7b2ea58701d340bb9d928efb7
Senthilvadivel-20 <senthilsubramaniyam20@gmail.com>
2021-08-10 23:43:04 +0530

    settings updated

--- ipl/settings.py
+++ ipl/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
    
 # ]
 
-STATICFILES_DIRS =[BASE_DIR / "static", ]
+# STATICFILES_DIRS =[BASE_DIR / "static", ]
 
 
 
